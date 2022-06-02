73ae1831076c8c41e05d9f287d81ee448efd30d6
egemenelz <egemenelz@gmail.com>
2022-06-02 02:43:47 -0400

    findByTitle and CreateNewNote Added

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
-const { findById, createNewNote, deleteNote } = require('../../lib/notes');
-const {notes} = require('../../db/db');
+const { findByTitle, createNewNote, deleteNote } = require('../../lib/notes');
+const { notes } = require('../../db/db');
 
 router.get('/notes/:title', (req, res) => {
+    console.log(notes);
+    const result = findByTitle(req.params.title, notes);
+    console.log(result);
 
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 })
 
+router.post('/notes', (req, res) => {
+    console.log(req.body);
+
+    // req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-// app.use('/api', appiRoutes);
+app.use('/api', appiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
