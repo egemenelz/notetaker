1a194d2e9e903b8bc95fbebb4a149c3e98791a3e
egemenelz <egemenelz@gmail.com>
2022-06-02 20:09:38 -0400

    Add Heroku

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
 const { findById, createNewNote, validateNote, filterByQuery } = require('../../lib/notes');
-const notes  = require('../../database/db.json');
+const notes = require('../../database/db.json');
 
 router.get('/notes', (req, res) => {
     console.log(notes)
     let results = notes;
-    if(req.query){
+    if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
 })
 
-router.get('/notes/:id', (req, res) => {  
-    console.log(notes)  
-    
+router.get('/notes/:id', (req, res) => {
+    console.log(notes)
+
     const result = findById(req.params.id, notes);
 
     if (result) {
@@ -30,13 +30,25 @@ router.post('/notes', (req, res) => {
 
     req.body.id = notes.length.toString();
 
-    if(!validateNote(req.body)){
+    if (!validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted.');
-    }else{
+    } else {
         const note = createNewNote(req.body, notes);
         res.json(note);
     }
 });
+
+router.delete('/notes/:id', (req, res) => {
+    const noteToDelete = findById(req.params.id, notes);
+
+    if(noteToDelete){
+        res.json(noteToDelete);
+    }else{
+        res.send(404);
+    }
+
+    notes.splice(notes.indexOf(noteToDelete), 1);
+})
 
 
 module.exports = router;
